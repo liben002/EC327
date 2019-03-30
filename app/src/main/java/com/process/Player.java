c7faad7dbf993e6130ce64c9aed649cb63af1bdd
John-Wilkins <jwilkinsspss@yahoo.com>
2019-03-30 14:48:07 -0400

    Filling out Constructors & Some Methods

--- app/src/main/java/com/process/Player.java
+++ app/src/main/java/com/process/Player.java
@@ -5,4 +5,10 @@ public class Player
     Track track;
     Piece[] tokens = new Piece[7];
     int tokensStart, tokensFinish;
+
+    //Default Constructor
+    public Player(int i)
+    {
+        //track =
+    }
 }
