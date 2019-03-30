dfd7f0a0482f80bd614bc3179163b19aac540b88
Benjamin Li <pharmashawarma@gmail.com>
2019-03-30 14:49:23 -0400

    mainactivity2, app crashes on startup

--- app/src/main/java/com/main/MainActivity.java
+++ app/src/main/java/com/main/MainActivity.java
@@ -1,8 +1,10 @@
 package com.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import com.example.ec327.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        Button startButton = (Button)findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GameScreenActivity.class));
+            }
+        });
     }
 }
