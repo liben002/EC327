1b5540d7dae3065e542d3632bf50f9a436d10e40
Benjamin Li <pharmashawarma@gmail.com>
2019-04-03 17:56:47 -0400

    Changed screen behavior, added roll template

--- app/src/main/java/com/main/GameScreenActivity.java
+++ app/src/main/java/com/main/GameScreenActivity.java
@@ -1,20 +1,32 @@
 package com.main;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import com.ur.Piece;
+
+import java.lang.Math;
 
 import com.example.ec327.R;
 
 public class GameScreenActivity extends AppCompatActivity {
 
+    private Piece[] player1Pieces = new Piece[7];
+    private Piece[] player2Piece = new Piece[7];
+
+    private boolean player1Turn = true;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
 
         Button rollButton = (Button)findViewById(R.id.rollButton);
         Button passButton = (Button)findViewById(R.id.passButton);
@@ -26,7 +38,25 @@ public class GameScreenActivity extends AppCompatActivity {
         findViewById(R.id.piece1_player1);
         simpleImageView.setImageResource(R.drawable.piece_1);*/
 
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int rollOutcome = (int) (Math.random() * 5);
 
+                // roll Test
+                Context context = getApplicationContext();
+                CharSequence text = "Roll: " + rollOutcome;
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                if(player1Turn) {
+                    //move the piece
+                } else {
+                    //move the piece
+                }
+            }
+        });
 
 
     }
--- app/src/main/java/com/main/MainActivity.java
+++ app/src/main/java/com/main/MainActivity.java
@@ -1,17 +1,22 @@
 package com.main;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import com.example.ec327.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         Button startButton = (Button)findViewById(R.id.startButton);
