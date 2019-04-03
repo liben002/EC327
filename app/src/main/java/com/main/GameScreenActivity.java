a0db514eab8a72aec4f7ca3ba9ab47b00e16a916
Briana Zhao <brianaz@bu.edu>
2019-04-03 16:32:14 -0400

    new GUI

--- app/src/main/java/com/main/GameScreenActivity.java
+++ app/src/main/java/com/main/GameScreenActivity.java
@@ -1,7 +1,11 @@
 package com.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.ec327.R;
 
@@ -11,5 +15,18 @@ public class GameScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        Button rollButton = (Button)findViewById(R.id.rollButton);
+        Button passButton = (Button)findViewById(R.id.passButton);
+
+
+        /*ImageView simpleImageView=(ImageView) findViewById(R.id.board);
+        simpleImageView.setImageResource(R.drawable.boardimage);
+
+        findViewById(R.id.piece1_player1);
+        simpleImageView.setImageResource(R.drawable.piece_1);*/
+
+
+
     }
 }
