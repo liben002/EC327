97633953163f73aa8478dbd19c97a0053996500b
Benjamin Li <pharmashawarma@gmail.com>
2019-04-03 16:47:11 -0400

    merge commit

--- app/src/main/java/com/main/GameScreenActivity.java
+++ app/src/main/java/com/main/GameScreenActivity.java
@@ -11,5 +11,10 @@ public class GameScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+
+
+
+
     }
 }
--- app/src/main/java/com/main/MainActivity.java
+++ app/src/main/java/com/main/MainActivity.java
@@ -12,12 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
 
         Button startButton = (Button)findViewById(R.id.startButton);
 
         startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, GameScreenActivity.class));
             }
