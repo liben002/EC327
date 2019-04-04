29be0fc922dc7137067653c4b3e968974d585419
John-Wilkins <jwilkinsspss@yahoo.com>
2019-04-03 20:31:21 -0400

    commit

--- app/src/main/java/com/main/GameScreenActivity.java
+++ app/src/main/java/com/main/GameScreenActivity.java
@@ -30,6 +30,7 @@ public class GameScreenActivity extends AppCompatActivity {
 
         //setup
         boolean isMulti = true;
+        System.out.println("Made it to square loc set up");
         Location[] squareLocs = new Location[20];
         try {
             for (int i = 0; i < 20; i++) {
@@ -40,91 +41,95 @@ public class GameScreenActivity extends AppCompatActivity {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
+            System.out.println("Made it past square loc set up");
+            for (int i = 0; i < 20; i++) {
+                //TODO get actual locations
+                squareLocs[i].setX(i);
+                squareLocs[i].setY(i);
+            }
+
+            Button rollButton = (Button) findViewById(R.id.rollButton);
+            Button passButton = (Button) findViewById(R.id.passButton);
+            ImageView test = (ImageView) findViewById(R.id.piece1_player1);
+
+            ImageView piece1_player1 = (ImageView) findViewById(R.id.piece1_player1);
+            ImageView piece2_player1 = (ImageView) findViewById(R.id.piece2_player1);
+            ImageView piece3_player1 = (ImageView) findViewById(R.id.piece3_player1);
+            ImageView piece4_player1 = (ImageView) findViewById(R.id.piece4_player1);
+            ImageView piece5_player1 = (ImageView) findViewById(R.id.piece5_player1);
+            ImageView piece6_player1 = (ImageView) findViewById(R.id.piece6_player1);
+            ImageView piece7_player1 = (ImageView) findViewById(R.id.piece7_player1);
+
+            ImageView piece1_player2 = (ImageView) findViewById(R.id.piece1_player2);
+            ImageView piece2_player2 = (ImageView) findViewById(R.id.piece2_player2);
+            ImageView piece3_player2 = (ImageView) findViewById(R.id.piece3_player2);
+            ImageView piece4_player2 = (ImageView) findViewById(R.id.piece4_player2);
+            ImageView piece5_player2 = (ImageView) findViewById(R.id.piece5_player2);
+            ImageView piece6_player2 = (ImageView) findViewById(R.id.piece6_player2);
+            ImageView piece7_player2 = (ImageView) findViewById(R.id.piece7_player2);
+
+            //Array of starting locations
+            Location[] pieces = new Location[14];
+
+            //Player 1 pieces
+            pieces[0].setX(piece1_player1.getLeft());
+            pieces[0].setY(piece1_player1.getTop());
+            pieces[1].setX(piece2_player1.getLeft());
+            pieces[1].setY(piece2_player1.getTop());
+            pieces[2].setX(piece3_player1.getLeft());
+            pieces[2].setY(piece3_player1.getTop());
+            pieces[3].setX(piece4_player1.getLeft());
+            pieces[3].setY(piece4_player1.getTop());
+            pieces[4].setX(piece5_player1.getLeft());
+            pieces[4].setY(piece5_player1.getTop());
+            pieces[5].setX(piece6_player1.getLeft());
+            pieces[5].setY(piece6_player1.getTop());
+            pieces[6].setX(piece7_player1.getLeft());
+            pieces[6].setY(piece7_player1.getTop());
+            //Player 2 pieces
+            pieces[7].setX(piece1_player2.getLeft());
+            pieces[7].setY(piece1_player2.getTop());
+            pieces[8].setX(piece2_player2.getLeft());
+            pieces[8].setY(piece2_player2.getTop());
+            pieces[9].setX(piece3_player2.getLeft());
+            pieces[9].setY(piece3_player2.getTop());
+            pieces[10].setX(piece4_player2.getLeft());
+            pieces[10].setY(piece4_player2.getTop());
+            pieces[11].setX(piece5_player2.getLeft());
+            pieces[11].setY(piece5_player2.getTop());
+            pieces[12].setX(piece6_player2.getLeft());
+            pieces[12].setY(piece6_player2.getTop());
+            pieces[13].setX(piece7_player2.getLeft());
+            pieces[13].setY(piece7_player2.getTop());
+
+            final Board board = new Board(squareLocs, pieces);
+
+            rollButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    int diceRoll = 0;
+                    for (int i = 0; i < 5; i++) {
+                        int individualRoll = (int) (Math.random() * 2);
+                        diceRoll += individualRoll;
+                    }
 
 
-        Button rollButton = (Button)findViewById(R.id.rollButton);
-        Button passButton = (Button)findViewById(R.id.passButton);
-        ImageView test = (ImageView)findViewById(R.id.piece1_player1);
+                    // roll Test
+                    Context context = getApplicationContext();
+                    CharSequence text = "Roll: " + diceRoll;
+                    int duration = Toast.LENGTH_SHORT;
 
-        ImageView piece1_player1 = (ImageView) findViewById(R.id.piece1_player1);
-        ImageView piece2_player1 = (ImageView) findViewById(R.id.piece2_player1);
-        ImageView piece3_player1 = (ImageView) findViewById(R.id.piece3_player1);
-        ImageView piece4_player1 = (ImageView) findViewById(R.id.piece4_player1);
-        ImageView piece5_player1 = (ImageView) findViewById(R.id.piece5_player1);
-        ImageView piece6_player1 = (ImageView) findViewById(R.id.piece6_player1);
-        ImageView piece7_player1 = (ImageView) findViewById(R.id.piece7_player1);
-
-        ImageView piece1_player2 = (ImageView) findViewById(R.id.piece1_player2);
-        ImageView piece2_player2 = (ImageView) findViewById(R.id.piece2_player2);
-        ImageView piece3_player2 = (ImageView) findViewById(R.id.piece3_player2);
-        ImageView piece4_player2 = (ImageView) findViewById(R.id.piece4_player2);
-        ImageView piece5_player2 = (ImageView) findViewById(R.id.piece5_player2);
-        ImageView piece6_player2 = (ImageView) findViewById(R.id.piece6_player2);
-        ImageView piece7_player2 = (ImageView) findViewById(R.id.piece7_player2);
-
-        //Array of starting locations
-        Location[] pieces = new Location[14];
-
-        //Player 1 pieces
-        pieces[0].setX(piece1_player1.getLeft());
-        pieces[0].setY(piece1_player1.getTop());
-        pieces[1].setX(piece2_player1.getLeft());
-        pieces[1].setY(piece2_player1.getTop());
-        pieces[2].setX(piece3_player1.getLeft());
-        pieces[2].setY(piece3_player1.getTop());
-        pieces[3].setX(piece4_player1.getLeft());
-        pieces[3].setY(piece4_player1.getTop());
-        pieces[4].setX(piece5_player1.getLeft());
-        pieces[4].setY(piece5_player1.getTop());
-        pieces[5].setX(piece6_player1.getLeft());
-        pieces[5].setY(piece6_player1.getTop());
-        pieces[6].setX(piece7_player1.getLeft());
-        pieces[6].setY(piece7_player1.getTop());
-        //Player 2 pieces
-        pieces[7].setX(piece1_player2.getLeft());
-        pieces[7].setY(piece1_player2.getTop());
-        pieces[8].setX(piece2_player2.getLeft());
-        pieces[8].setY(piece2_player2.getTop());
-        pieces[9].setX(piece3_player2.getLeft());
-        pieces[9].setY(piece3_player2.getTop());
-        pieces[10].setX(piece4_player2.getLeft());
-        pieces[10].setY(piece4_player2.getTop());
-        pieces[11].setX(piece5_player2.getLeft());
-        pieces[11].setY(piece5_player2.getTop());
-        pieces[12].setX(piece6_player2.getLeft());
-        pieces[12].setY(piece6_player2.getTop());
-        pieces[13].setX(piece7_player2.getLeft());
-        pieces[13].setY(piece7_player2.getTop());
-
-        final Board board = new Board(squareLocs,pieces);
-
-        rollButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                int diceRoll = 0;
-                for (int i = 0; i < 5; i++) {
-                    int individualRoll = (int) (Math.random() * 2);
-                    diceRoll += individualRoll;
-                }
-
-
-
-                // roll Test
-                Context context = getApplicationContext();
-                CharSequence text = "Roll: " + diceRoll;
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-                while(gameState == 0) {
-                    for(int i = 0; i < 14; i ++) {
-                        System.out.println(board.getPieceScreenLoc(i));
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    while (gameState == 0) {
+                        for (int i = 0; i < 14; i++) {
+                            System.out.println(board.getPieceScreenLoc(i));
+                        }
                     }
                 }
-            }
-        });
+            });
 
 
+        }
     }
 }
