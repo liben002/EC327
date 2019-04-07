b5e1b55a9bd9b0b0da854cba63f9de1412721e4f
Benjamin Li <pharmashawarma@gmail.com>
2019-04-07 16:31:33 -0400

    hid toolbar, changed variable names to match cC, removed pass turn button

--- app/src/main/java/com/main/WinScreenActivity.java
+++ app/src/main/java/com/main/WinScreenActivity.java
@@ -15,8 +15,14 @@ public class WinScreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // hide toolbar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // hide navigation bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         setContentView(R.layout.activity_win_screen);
 
         Button playagainButton = findViewById(R.id.playagainButton);
@@ -38,5 +44,20 @@ public class WinScreenActivity extends Activity {
                 startActivity(homeIntent);
             }
         });
+    }
+
+    // overriding window change for navigation bar
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
